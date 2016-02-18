7fe3a58ca5630e9abb05f3aab5e86d21acc316d9
yoshiaki.sakota <yoshiaki.sakota@dena.com>
2016-02-18 20:32:32 +0900

    Make auto texture importer

---
+++ Assets/Editor/Dither16bitModifier.cs
@@ -0,0 +1,112 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace Editor.UI
+{
+	/// <summary>
+	/// ディザリング処理クラス。TextureImportDefinition.csを玄関として、必要なテクスチャの場合こちらの処理が読まれます。
+	/// </summary>
+	public class Dither16bitModifier
+	{
+		/// <summary>
+		/// 一旦減色・ディザ処理前に32bitのフルカラーにプリプロセスで変更.
+		/// </summary>
+		public static void SetPreDitherImportSettings(TextureImporter importer)
+		{
+			importer.textureFormat = TextureImporterFormat.RGBA32;
+		}
+
+		public static void Process16bitDither(Texture2D texture, TextureImporter importer)
+		{
+			var pixels = _FloydSteinberg16bitsDither(texture.GetPixels(), texture.width, texture.height);
+			texture.SetPixels(pixels);
+			EditorUtility.CompressTexture(texture, TextureFormat.RGBA4444, TextureCompressionQuality.Best);
+			//減色・ディザ処理後に16bitにフォーマット設定を変更.
+			importer.textureFormat = TextureImporterFormat.Automatic16bit;
+		}
+
+		/// <summary>
+		/// 16bit減色>FloydSteinbergアルゴリズムのディザ処理.
+		/// </summary>
+		/// <returns>Color[].</returns>
+		/// <param name="pixels">Pixels.</param>
+		/// <param name="texw">Texw.</param>
+		/// <param name="texh">Texh.</param>
+		private static Color[] _FloydSteinberg16bitsDither(Color[] pixels, int texw, int texh)
+		{
+			var offs = 0;
+			var k1Per15 = 1.0f / 15.0f;
+			var k1Per16 = 1.0f / 16.0f;
+			var k3Per16 = 3.0f / 16.0f;
+			var k5Per16 = 5.0f / 16.0f;
+			var k7Per16 = 7.0f / 16.0f;
+
+			for (var y = 0; y < texh; y++)
+			{
+				for (var x = 0; x < texw; x++)
+				{
+					float a = pixels [offs].a;
+					float r = pixels [offs].r;
+					float g = pixels [offs].g;
+					float b = pixels [offs].b;
+
+					var a2 = Mathf.Clamp01(Mathf.Floor(a * 16) * k1Per15);
+					var r2 = Mathf.Clamp01(Mathf.Floor(r * 16) * k1Per15);
+					var g2 = Mathf.Clamp01(Mathf.Floor(g * 16) * k1Per15);
+					var b2 = Mathf.Clamp01(Mathf.Floor(b * 16) * k1Per15);
+
+					var ae = a - a2;
+					var re = r - r2;
+					var ge = g - g2;
+					var be = b - b2;
+
+					pixels [offs].a = a2;
+					pixels [offs].r = r2;
+					pixels [offs].g = g2;
+					pixels [offs].b = b2;
+
+					var n1 = offs + 1;
+					var n2 = offs + texw - 1;
+					var n3 = offs + texw;
+					var n4 = offs + texw + 1;
+
+					if (x < texw - 1)
+					{
+						pixels [n1].a += ae * k7Per16;
+						pixels [n1].r += re * k7Per16;
+						pixels [n1].g += ge * k7Per16;
+						pixels [n1].b += be * k7Per16;
+					}
+
+					if (y < texh - 1)
+					{
+						pixels [n3].a += ae * k5Per16;
+						pixels [n3].r += re * k5Per16;
+						pixels [n3].g += ge * k5Per16;
+						pixels [n3].b += be * k5Per16;
+
+						if (x > 0)
+						{
+							pixels [n2].a += ae * k3Per16;
+							pixels [n2].r += re * k3Per16;
+							pixels [n2].g += ge * k3Per16;
+							pixels [n2].b += be * k3Per16;
+						}
+
+						if (x < texw - 1)
+						{
+							pixels [n4].a += ae * k1Per16;
+							pixels [n4].r += re * k1Per16;
+							pixels [n4].g += ge * k1Per16;
+							pixels [n4].b += be * k1Per16;
+						}
+					}
+
+					offs++;
+				}
+			}
+
+			return pixels;
+		}
+	}
+}
---
+++ Assets/Editor/UITextureImporter.cs
@@ -0,0 +1,124 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+using System.IO;
+
+namespace Editor.UI
+{
+	/// <summary>
+	/// UI系のテクスチャー設定の自動化処理です。
+	/// テクスチャーをインポートするパス名の末尾が/Battle/Default/xx.pngとなっている場合は
+	/// Battleタグをつけた後、16bitフォーマットに変換し、ディザ処理を自動で行います。
+	/// もし/Battle/Compressed/xx.pngとなっている場合はBattleタグをつけた後、Compressedフォーマットに変換します。
+	/// その他のUIの基本的なテクスチャー設定も自動で行います。
+	/// </summary>
+	public class UITextureImporter : AssetPostprocessor
+	{
+		private bool            _isDitherAsset = false;
+		private TextureImporter _importer;
+
+		private const int    ANISO_LEVEL               = 0;
+		//UIディレクトリ.
+		private const string UI_ROOT_DIRECTORY         = "Assets/Images/";
+		//UIディレクトリ内の16bitとディザをかけるディレクトリ.
+		private const string DITHER_16BIT_DIRECTORY    = "Dither";
+		//UIディレクトリ内のディザをかけない16bitディレクトリ.
+		private const string DEFAULT_16BIT_DIRECTORY   = "Default";
+		//UIディレクトリ内のCompressed設定にするディレクトリ.
+		private const string COMPRESSE_DIRECTORY       = "Compressed";
+		private const string DIRECTORY_WARNING_MESSAGE = "UIテクスチャーは" + DEFAULT_16BIT_DIRECTORY + "か" + DITHER_16BIT_DIRECTORY + "か" + COMPRESSE_DIRECTORY + "ディレクトリに入れてください";
+
+		public enum MAX_TEXTURE_SIZE
+		{
+			Size16   = 16,
+			Size32   = 32,
+			Size64   = 64,
+			Size128  = 128,
+			Size256  = 256,
+			Size512  = 512,
+			Size1024 = 1024,
+			Size2048 = 2048
+		}
+
+		/// <summary>
+		/// アセットインポート開始前にcall.
+		/// </summary>
+		void OnPreprocessTexture()
+		{
+			if (assetPath.StartsWith(UI_ROOT_DIRECTORY) == false)
+			{
+				return;
+			}
+
+			string directory = Path.GetDirectoryName(assetPath);
+
+			if (!directory.EndsWith(DEFAULT_16BIT_DIRECTORY) & !directory.EndsWith(DITHER_16BIT_DIRECTORY) & !directory.EndsWith(COMPRESSE_DIRECTORY))
+			{
+				Debug.LogWarning(DIRECTORY_WARNING_MESSAGE);
+				return;
+			}
+
+			_importer = (TextureImporter)assetImporter;
+
+			//インポートセッティング処理.
+			_SetImportSettings(Path.GetFileName(Path.GetDirectoryName(directory)));
+
+			//フォーマット処理.
+			if (directory.EndsWith(DITHER_16BIT_DIRECTORY))
+			{
+				_isDitherAsset = true;
+				Dither16bitModifier.SetPreDitherImportSettings(_importer);
+			}
+			else if (directory.EndsWith(DEFAULT_16BIT_DIRECTORY))
+			{
+				_Set16bitFormat();
+			}
+			else if (directory.EndsWith(COMPRESSE_DIRECTORY))
+			{
+				_SetCompressedFormat();
+			}
+		}
+
+		/// <summary>
+		/// アセットインポート完了直後にcall.
+		/// </summary>
+		/// <param name="texture">Texture.</param>
+		void OnPostprocessTexture(Texture2D texture)
+		{
+			if (!_isDitherAsset)
+			{
+				return;
+			}
+
+			Dither16bitModifier.Process16bitDither(texture, _importer);
+		}
+
+		private void _SetImportSettings(string packingTag)
+		{
+			_importer.spritePackingTag = packingTag;
+			_importer.textureType = TextureImporterType.Advanced;
+			_importer.alphaIsTransparency = true;
+			_importer.generateCubemap = TextureImporterGenerateCubemap.None;
+			_importer.isReadable = false;
+			_importer.lightmap = false;
+			_importer.normalmap = false;
+			_importer.spriteImportMode = SpriteImportMode.Single;
+			_importer.npotScale = TextureImporterNPOTScale.None;
+			_importer.mipmapEnabled = false;
+			_importer.wrapMode = TextureWrapMode.Clamp;
+			_importer.filterMode = FilterMode.Bilinear;
+			_importer.anisoLevel = ANISO_LEVEL;
+			_importer.maxTextureSize = (int)MAX_TEXTURE_SIZE.Size2048;
+		}
+
+		private void _Set16bitFormat()
+		{
+			_importer.textureFormat = TextureImporterFormat.Automatic16bit;
+		}
+
+		private void _SetCompressedFormat()
+		{
+			_importer.textureFormat = TextureImporterFormat.AutomaticCompressed;
+		}
+	}
+}
